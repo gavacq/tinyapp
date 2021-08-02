85ec02d7868fc4b02e0ea474e863d19a75585e86
Gavin Acquroff <gavina352@gmail.com>
2021-08-02 23:38:54 +0000

    Add 404 page, redirect to longURL.

--- express_server.js
+++ express_server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const PORT = 8080;
 
@@ -10,13 +11,14 @@ const urlDatabase = {
 };
 
 const generateRandomString = () => {
-  return Math.random().toString(36).replace(/[^a-z0-9]+/g, '').substr(0, 5);
+  return Math.random().toString(36).replace(/[^a-z0-9]+/g, '').substr(0, 6);
 };
 
 app.use(express.urlencoded({extended: true}));
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.send("Hello!asdfasdfas");
 });
 
 app.get("/urls", (req, res) => {
@@ -26,7 +28,11 @@ app.get("/urls", (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const tinyURL = generateRandomString();
+  urlDatabase[tinyURL] = req.body.longURL;
+  console.log(`New URL stored: {${tinyURL} : ${urlDatabase[tinyURL]}}`);
+  
+  res.redirect(`urls/${tinyURL}`);         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/new", (req, res) => {
@@ -39,6 +45,23 @@ app.get("/urls/:shortURL", (req, res) => {
     longURL: urlDatabase[req.params.shortURL]
   };
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  if (!longURL) {
+    // TODO: not DRY
+    res.status(404).render("404_error");
+    
+    return;
+  }
+
+  res.redirect(longURL);
+});
+
+// 404 handler
+app.get("*", (req, res) => {
+  res.status(404).render("404_error");
 });
 
 app.listen(PORT, () => {
