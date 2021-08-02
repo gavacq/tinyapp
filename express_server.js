bc10bbc5db0607b4f18d7baf70effc1431d08cf9
Gavin Acquroff <gavina352@gmail.com>
2021-08-02 22:03:26 +0000

    Add POST method to /urls. Create generateRandomString().

--- express_server.js
+++ express_server.js
@@ -9,6 +9,12 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const generateRandomString = () => {
+  return Math.random().toString(36).replace(/[^a-z0-9]+/g, '').substr(0, 5);
+};
+
+app.use(express.urlencoded({extended: true}));
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -16,6 +22,11 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/new", (req, res) => {
